f3996e29ec4f04a8ae5a4ef89d913bf6f9840197
olivervillalobos <71286508+olivervillalobos@users.noreply.github.com>
2020-12-02 22:47:14 -0800

    Update programa.s

--- programa.s
+++ programa.s
@@ -1,23 +1,12 @@
-.data
-
-/* Data segment: define our message string and calculate its length. */
-msg:
-    .ascii        "Hello, ARM64!\n"
-len = . - msg
-
-.text
-
-/* Our application's entry point. */
-.globl _start
+.global _start
 _start:
-    /* syscall write(int fd, const void *buf, size_t count) */
-    mov     x0, #1      /* fd := STDOUT_FILENO */
-    ldr     x1, =msg    /* buf := msg */
-    ldr     x2, =len    /* count := len */
-    mov     w8, #64     /* write is syscall #64 */
-    svc     #0          /* invoke syscall */
-
-    /* syscall exit(int status) */
-    mov     x0, #0      /* status := 0 */
-    mov     w8, #93     /* exit is syscall #1 */
-    svc     #0          /* invoke syscall */
+  MOV R7, #4
+  MOV R0, #1
+  MOV R2, #12
+  LDR R1, =string
+  SWI 0
+  MOV R7, #1
+  SWI 0
+  .data
+string:
+  .ascii "Hello Worldn"
