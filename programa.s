eb2d6c467a9a95e68ebce6e65dac2cb744ded749
olivervillalobos <71286508+olivervillalobos@users.noreply.github.com>
2020-12-03 15:22:51 -0800

    Update programa.s

--- programa.s
+++ programa.s
@@ -1,12 +1,23 @@
-.global _start
+.data
+
+/* Segmento de Datos: define el mensaje y calcula el tramaño de la cadena. */
+msg:
+    .ascii        "Hello, ARM64!\n"
+len = . - msg
+
+.text
+
+/* Cuerpo del programa para desplegar la variable anterior. */
+.globl _start
 _start:
-  MOV R7, #4
-  MOV R0, #1
-  MOV R2, #12
-  LDR R1, =string
-  SWI 0
-  MOV R7, #1
-  SWI 0
-  .data
-string:
-  .ascii "Hello Worldn"
+    /* syscall write(int fd, const void *buf, size_t count) */
+    mov     x0, #1      /* fd := STDOUT_FILENO */
+    ldr     x1, =msg    /* buf := msg */
+    ldr     x2, =len    /* count := len */
+    mov     w8, #64     /* write is syscall #64 */
+    svc     #0          /* invoke syscall */
+
+    /* syscall exit(int status) */
+    mov     x0, #0      /* status := 0 */
+    mov     w8, #93     /* exit is syscall #1 */
+    svc     #0          /* invoke syscall */
